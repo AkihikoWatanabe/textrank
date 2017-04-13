02e86d32b7ed226d8464c247718db093e77448bb
AkihikoWatanabe <watanabe@lr.pi.titech.ac.jp>
2017-04-13 14:15:21 +0900

    add license, setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='textrank',
+    version='0.0.1',
+    author='Akihiko Watanabe',
+    author_email='watanabe@lr.pi.titech.ac.jp',
+    install_requires=[
+        'numpy'
+    ]
+)
